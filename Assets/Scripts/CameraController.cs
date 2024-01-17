52585a293253e00b07a5911ff257712356752efb
ShaoKuy <iqtanawad@gmail.com>
2024-01-17 13:08:32 +0700

    Add Zoom System

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CameraController : MonoBehaviour
 {
@@ -10,8 +11,18 @@ public class CameraController : MonoBehaviour
     [Header("Move")]
     [SerializeField] private float moveSpeed;
 
+    [SerializeField] private Transform corner1;
+    [SerializeField] private Transform corner2;
+
     [SerializeField] private float xInput;
     [SerializeField] private float zInput;
+
+    [Header("Zoom")] 
+    [SerializeField] private float zoomSpeed;
+    [SerializeField] private float zoomModifier;
+    [SerializeField] private float minZoomDist;
+    [SerializeField] private float maxZoomDist;
+    [SerializeField] private float dist;
 
     public static CameraController instance;
 
@@ -25,12 +36,17 @@ public class CameraController : MonoBehaviour
     void Start()
     {
         moveSpeed = 50;
+        zoomSpeed = 25;
+        minZoomDist = 15;
+        maxZoomDist = 50;
+
     }
 
     // Update is called once per frame
     void Update()
     {
         MoveByKeyboard();
+        Zoom();
     }
 
     private void MoveByKeyboard()
@@ -38,7 +54,34 @@ public class CameraController : MonoBehaviour
         xInput = Input.GetAxis("Horizontal");
         zInput = Input.GetAxis("Vertical");
 
-        Vector3 dir = (transform.forward * zInput) + (transform.right * zInput);
+        Vector3 dir = (transform.forward * zInput) + (transform.right * xInput);
         transform.position += dir * moveSpeed * Time.deltaTime;
+        transform.position = clamp(corner1.position, corner2.position);
+    }
+
+    private Vector3 clamp(Vector3 lowerLeft, Vector3 topRight)
+    {
+        Vector3 pos = new Vector3(Mathf.Clamp(transform.position.x, lowerLeft.x, topRight.x),
+            transform.position.y, Mathf.Clamp(transform.position.z, lowerLeft.z, topRight.z));
+        return pos;
+    }
+
+    private void Zoom()
+    {
+        zoomModifier = Input.GetAxis("Mouse ScrollWheel");
+        if (Input.GetKey(KeyCode.Z))
+            zoomModifier = 0.01f;
+        if (Input.GetKey(KeyCode.X))
+            zoomModifier = -0.01f;
+
+        dist = Vector3.Distance(transform.position, cam.transform.position);
+
+        if (dist < minZoomDist && zoomModifier > 0f)
+            return; //too close
+        else if (dist > maxZoomDist && zoomModifier < 0f)
+            return; //too far
+
+        cam.transform.position += cam.transform.forward * zoomModifier * zoomSpeed;
+
     }
 }
